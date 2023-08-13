511bd156704a2eb518020ca3d8c38c3c4c4393b0
Ibadat <ibadatafridi7@gmail.com>
2023-08-13 06:45:12 -0400

    Pushing To Remote Git Repository

---
+++ script.js
@@ -0,0 +1,12 @@
+let eyeicon = document.getElementById("icon");
+let password = document.getElementById("password");
+
+eyeicon.onclick = () => {
+  if (password.type == "password") {
+    password.type = "text";
+    eyeicon.src = "eye-open.png";
+  } else {
+    password.type = "password";
+    eyeicon.src = "eye-close.png";
+  }
+};
